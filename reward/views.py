6d725207dfcb9b4bc60216382df626674b02844e
joseck ogachi <jogachi4@gmail.com>
2019-03-18 16:53:35 +0300

    add changes to views.py

--- reward/views.py
+++ reward/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+  id = request.user.id
+  profile = Profile.objects.get(user=id)
