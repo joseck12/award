5bf797eed57043b68b46d40717d37c925f71a9e8
joseck ogachi <jogachi4@gmail.com>
2019-03-18 17:08:17 +0300

    add new content to urls.py

--- reward/urls.py
+++ reward/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^search/', views.search,name = 'search'),
+    url(r'^new/project$',views.newproject, name='newproject'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
 
 ]
