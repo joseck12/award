9d493c17ddf95fce19fbd0aff287e178ed1dfadc
joseck ogachi <jogachi4@gmail.com>
2019-03-18 17:03:42 +0300

    add new content to views.py

--- reward/views.py
+++ reward/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-import datetime as dt
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
   id = request.user.id
   profile = Profile.objects.get(user=id)
-def news_of_day(request):
-    date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+
+
+  return render(request, 'index.html',{'profile':profile})
+
+
+@login_required(login_url='/accounts/login/')
+def myprojects(request):
+  id = request.user.id
+  profile = Profile.objects.get(user=id)
+
+  projects = Project.objects.all().order_by('-pub_date')
+
+  return render(request, 'myproject.html',{'projects':projects,'profile':profile})
+def convert_dates(dates):
+
+@login_required(login_url='/accounts/login/')
+def password(request):
+  id = request.user.id
+  profile = Profile.objects.get(user=id)
+
+
+  return render(request, 'password.html',{'profile':profile})
