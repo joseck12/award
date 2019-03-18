4f6e1e3bd5a284fda77b551910ce2836e4c90a8f
joseck ogachi <jogachi4@gmail.com>
2019-03-18 16:56:07 +0300

    add content to views.py

--- reward/views.py
+++ reward/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-
+import datetime as dt
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
   id = request.user.id
   profile = Profile.objects.get(user=id)
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
